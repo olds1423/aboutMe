bd42e335e155c892c26245b54cdf6e44c8e7feca
olds1423 <olds1423@gmail.com>
2016-05-26 19:14:11 -0700

    First alert

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-//The Javascript portion of the About Me project 
+//The Javascript portion of the About Me project
+var questionOne = prompt ("Did you learn anything?").toLowerCase();
+if (questionOne === "yes" || "y"){
+  alert ("We're getting somewhere!");
+} else {
+  alert ("Poorly done.");
+}
