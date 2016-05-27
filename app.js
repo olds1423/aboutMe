abdfc205ced378be4a044d2e3956563e45931c60
olds1423 <olds1423@gmail.com>
2016-05-26 20:16:32 -0700

    Removed redudency in JS that was causing a n error

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
 //The Javascript portion of the About Me project
 function quiz() {
-    if (confirm("Please answer the following questions with simple Y/N answers.")){
-      var questionOne = prompt ("Did you learn anything?").toLowerCase();
+    confirm("Please answer the following questions with simple Y/N answers.")
+
+    var questionOne = prompt ("Was I born in Washington?").toLowerCase();
       //Is there a reason to define the wrong answers? Why would it matter if the user said No or hazaah? Both are wrong?
-      if (questionOne === "yes" || "y"){
-        alert ("We're getting somewhere!");
-      } else {
-        alert ("Poorly done.\n Try again.")// && break;
-      }
-    }
+        if (questionOne === "yes" || questionOne === "y"){
+          alert ("We're getting somewhere!");
+        } else {
+          alert ("Poorly done.\n Try again.");
+        }
+      // var questionTwo = prompt ("Did I attend an amazing coding accelerator?").toLowerCase();
+      //   if (questionTwo === "yes" || "y") {
+      //     alert ("We're getting somewhere!");
+      //   } else {
+      //     alert ("Poorly done.\n Try again.");
+      //   }
+
 }
