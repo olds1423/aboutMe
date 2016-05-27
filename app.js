d04a51f4fcdb48a58aac5a6f1bc8112b8538c6d8
olds1423 <olds1423@gmail.com>
2016-05-26 19:31:25 -0700

    Quiz button added with surprising success.

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 //The Javascript portion of the About Me project
-var questionOne = prompt ("Did you learn anything?").toLowerCase();
-if (questionOne === "yes" || "y"){
-  alert ("We're getting somewhere!");
-} else {
-  alert ("Poorly done.");
+function quiz() {
+    if (confirm("Please answer the following questions with simple Y/N answers.")){
+      var questionOne = prompt ("Did you learn anything?").toLowerCase();
+      if (questionOne === "yes" || "y"){
+        alert ("We're getting somewhere!");
+      } else {
+        alert ("Poorly done.");
+      }
+    }
 }
