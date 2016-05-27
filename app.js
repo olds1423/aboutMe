88346b6d21756f37d9207e60a0c383c15031eb87
olds1423 <olds1423@gmail.com>
2016-05-26 20:21:55 -0700

    Added remaining questions and added required README

--- app.js
+++ app.js
@@ -4,16 +4,27 @@ function quiz() {
 
     var questionOne = prompt ("Was I born in Washington?").toLowerCase();
       //Is there a reason to define the wrong answers? Why would it matter if the user said No or hazaah? Both are wrong?
-        if (questionOne === "yes" || questionOne === "y"){
-          alert ("We're getting somewhere!");
-        } else {
-          alert ("Poorly done.\n Try again.");
-        }
-      // var questionTwo = prompt ("Did I attend an amazing coding accelerator?").toLowerCase();
-      //   if (questionTwo === "yes" || "y") {
-      //     alert ("We're getting somewhere!");
-      //   } else {
-      //     alert ("Poorly done.\n Try again.");
-      //   }
-
+      if (questionOne === "yes" || questionOne === "y"){
+        alert ("We're getting somewhere!");
+      } else {
+        alert ("Poorly done.\nTry again.");
+      }
+    var questionTwo = prompt ("Did I attend an amazing coding accelerator?").toLowerCase();
+      if (questionTwo === "yes" || questionTwo === "y") {
+        alert ("We're getting somewhere!");
+      } else {
+        alert ("Poorly done.\nTry again.");
+      }
+    var questionThree = prompt ("Was that coding accelerator the start of something new?").toLowerCase();
+      if (questionThree === "yes" || questionThree === "y") {
+        alert ("We're getting somewhere!");
+      } else {
+        alert ("Poorly done.\nTry again.");
+      }
+    var questionFour = prompt ("Was that something new....Skynet?!").toLowerCase();
+      if (questionFour === "yes" || questionFour === "y") {
+        alert ("We're getting somewhere!");
+      } else {
+        alert ("Poorly done.\nTry again.");
+      }
 }
