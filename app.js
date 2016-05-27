56779b43115b888ed7e2c6278f40cd8d23efc31f
olds1423 <olds1423@gmail.com>
2016-05-26 19:48:00 -0700

    Adding the list of quiz items, incorporating more JS

--- app.js
+++ app.js
@@ -2,10 +2,11 @@
 function quiz() {
     if (confirm("Please answer the following questions with simple Y/N answers.")){
       var questionOne = prompt ("Did you learn anything?").toLowerCase();
+      //Is there a reason to define the wrong answers? Why would it matter if the user said No or hazaah? Both are wrong?
       if (questionOne === "yes" || "y"){
         alert ("We're getting somewhere!");
       } else {
-        alert ("Poorly done.");
+        alert ("Poorly done.\n Try again.")// && break;
       }
     }
 }
