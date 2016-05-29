1708e5fbfeaf0c3c2ab9942362bd66adc54f26a1
olds1423 <olds1423@gmail.com>
2016-05-28 17:34:17 -0700

    Fixed amountCorrect, sort of fixed questionFive

--- app.js
+++ app.js
@@ -8,36 +8,51 @@ function quiz() {
       //Is there a reason to define the wrong answers? Why would it matter if the user said No or hazaah? Both are wrong?
       if (questionOne === "yes" || questionOne === "y"){
         alert ("We're getting somewhere!");
+        amountCorrect += 1;
+        console.log(amountCorrect);
       } else {
         alert ("Poorly done.\nTry again.");
       }
     var questionTwo = prompt ("Did I attend an amazing coding accelerator?").toLowerCase();
       if (questionTwo === "yes" || questionTwo === "y") {
         alert ("We're getting somewhere!");
+        amountCorrect += 1;
       } else {
         alert ("Poorly done.\nTry again.");
       }
     var questionThree = prompt ("Was that coding accelerator the start of something new?").toLowerCase();
       if (questionThree === "yes" || questionThree === "y") {
         alert ("We're getting somewhere!");
+        amountCorrect += 1;
       } else {
         alert ("Poorly done.\nTry again.");
       }
     var questionFour = prompt ("Was that something new....Skynet?!").toLowerCase();
       if (questionFour === "yes" || questionFour === "y") {
         alert ("We're getting somewhere!");
+        amountCorrect += 1;
       } else {
         alert ("Poorly done.\nTry again.");
       }
 //Start of the first real logical questions below
-    var questionFive = parseInt(prompt ("What is my favorite number?"));
-      while (questionFive !== favoriteNumber) {
-        if (questionFive >= favoriteNumber) {
-        questionFive = parseInt(prompt ("Too high!"));
-        }
-        else if (questionFive <= favoriteNumber){
-        questionFive = parseInt(prompt ("Too low!"));
-        }
-      }
+        console.log(amountCorrect);
+        var counter = 3
+        var questionFive = parseInt(prompt ("What is my favorite number?"));
+          while (questionFive !== favoriteNumber && counter >= 0) {
+            console.log('running while loop');
+            if (questionFive >= favoriteNumber) {
+              console.log('inside while/if');
+              questionFive = parseInt(prompt ("Too high!"));
+              --counter;
+              console.log(counter);
+            }
+            else if (questionFive <= favoriteNumber){
+              console.log('inside while/elseif');
+              questionFive = parseInt(prompt ("Too low!"));
+              --counter;
+              console.log(counter);
+            }
+          }
+    // }
     alert ("Nice!")
 }
